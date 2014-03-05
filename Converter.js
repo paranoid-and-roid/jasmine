d1622a7bef6a865c090427590e84d5bbbc838fcc
PJ Dunlevy <yorko9999@gmail.com>
2014-03-05 17:17:03 +0000

    commit with Converters added

---
+++ Converter.js
@@ -0,0 +1,18 @@
+var Converter = (function () {
+    // private stuff goes here
+
+
+    return {
+
+        convertFromImperialToMetric: function (weight) {
+
+        },
+
+        convertFromMetricToImperial: function (weight) {
+
+        }
+
+    }
+
+
+})();
